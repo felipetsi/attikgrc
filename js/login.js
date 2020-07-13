dfc3845cd0f60965313786b704a403510985b7fd
Felipe Pereira <felipe.pereira@attik.com.br>
2020-07-12 21:03:50 -0300

    first commit

--- js/login.js
+++ js/login.js
@@ -3,6 +3,7 @@ $(function() {
 	if (localStorage.chkbx && localStorage.chkbx != '') {
 		$('#remember_me').attr('checked', 'checked');
 		$('#login').val(localStorage.usrname);
+		$('#password').val(localStorage.password);
 	} else {
 		$('#remember_me').removeAttr('checked');
 		$('#login').val('');
@@ -13,9 +14,11 @@ $(function() {
 		if ($('#remember_me').is(':checked')) {
 			// save login
 			localStorage.usrname = $('#login').val();
+			localStorage.password = $('#password').val();
 			localStorage.chkbx = $('#remember_me').val();
 		} else {
 			localStorage.usrname = '';
+			localStorage.password = '';
 			localStorage.chkbx = '';
 		}
 	});
